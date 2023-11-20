38c6d5310af5201bad4e6a6d21c4b629d7147867
Ethan <holterethan@gmail.com>
2023-11-20 01:21:44 -0600

    modifications to support custom hardware

--- include/motor_driver.h
+++ include/motor_driver.h
@@ -2,15 +2,25 @@
    Motor driver function definitions - by James Nugen
    *************************************************************/
 
-#ifdef L298_MOTOR_DRIVER
-  #define RIGHT_MOTOR_BACKWARD 5
-  #define LEFT_MOTOR_BACKWARD  6
-  #define RIGHT_MOTOR_FORWARD  9
-  #define LEFT_MOTOR_FORWARD   10
-  #define RIGHT_MOTOR_ENABLE 12
-  #define LEFT_MOTOR_ENABLE 13
-#endif
+#ifndef MOTOR_DRIVER_H
+#define MOTOR_DRIVER_H
+
+#define RIGHT_FWD_MOTOR 2 // TODO arbitrarily assigned pin number 
+#define RIGHT_MID_MOTOR 3 // TODO arbitrarily assigned pin number
+#define RIGHT_BCK_MOTOR 4 // TODO arbitrarily assigned pin number
+
+#define LEFT_FWD_MOTOR 5 // TODO arbitrarily assigned pin number
+#define LEFT_MID_MOTOR 6 // TODO arbitrarily assigned pin number
+#define LEFT_BCK_MOTOR 7 // TODO arbitrarily assigned pin number
+
+#define RIGHT_DIR 22 // TODO arbitrarily assigned pin number
+#define LEFT_DIR 23 // TODO arbitrarily assigned pin number
+
+#define RIGHT_ENABLE 24 // TODO arbitrarily assigned pin number
+#define LEFT_ENABLE 25 // TODO arbitrarily assigned pin number
 
 void initMotorController();
 void setMotorSpeed(int i, int spd);
 void setMotorSpeeds(int leftSpeed, int rightSpeed);
+
+#endif
