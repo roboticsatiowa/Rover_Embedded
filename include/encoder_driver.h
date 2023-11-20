2ab23f1433bc301ea3cbf573d1376002f064b317
Ethan <holterethan@gmail.com>
2023-11-20 17:17:06 -0600

    Update encoder driver functions and ISRs

--- include/encoder_driver.h
+++ include/encoder_driver.h
@@ -5,17 +5,14 @@
 #ifndef ENCODER_DRIVER_H
 #define ENCODER_DRIVER_H
 
-// below can be changed, but should be PORTD pins;
-// otherwise additional changes in the code are required
-#define LEFT_ENC_PIN_A 2  // TODO arbitrarily assigned pin number
-#define LEFT_ENC_PIN_B 3  // TODO arbitrarily assigned pin number
-
-// below can be changed, but should be PORTC pins
-#define RIGHT_ENC_PIN_A 4  // TODO arbitrarily assigned pin number
-#define RIGHT_ENC_PIN_B 5  // TODO arbitrarily assigned pin number
 
 long readEncoder(int i);
 void resetEncoder(int i);
 void resetEncoders();
+void ISR(int addr);
+void BASEMOTOR_ISR();
+void WRIST_INCLINATION_ISR();
+void WRIST_ROTATION_ISR();
+void GRIPPER_ISR();
 
 #endif
