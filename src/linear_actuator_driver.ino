375d63856e33bc03c2cad72b9361b37c1c95785b
kto <kto@uiowa.edu>
2024-02-09 02:22:31 -0600

    yes

--- src/linear_actuator_driver.ino
+++ src/linear_actuator_driver.ino
@@ -14,40 +14,8 @@ void init_linear_actuator_controller()
 
 void set_linear_actuator_speed(int addr, int velocity)
 {
-   //LINEAR_ACTUATOR_SERIAL.write(velocity);
    LINEAR_ACTUATOR_SERIAL.write(addr << 7 | lerp(velocity, -255, 255, 1, 127)); //first bit is address, rest is velocity
+    //does this work for motor 2?
 }
 
-
-// void init_linear_actuator_controller()
-// {
-//     LINEAR_ACTUATOR_SERIAL.begin(9600);
-// }
-
-// void set_linear_actuator_speed(int addr, int power)
-// {
-//     power = lerp(power, -255, 255, 1, 127);
-
-
-//     byte command, magnitude;
-//     power = constrain(power, -127, 127);
-//     magnitude = abs(power) >> 1;
-    
-//     //commands 0 - 127 runs motor 1, 64 stop
-//     if (addr == 0)
-//     {
-//         command = power < 0 ? 63 - magnitude : 64 + magnitude;
-//     }
-
-//     //commands 128-255 runs motor 2, 192 stop
-//     else if (addr == 1)
-//     {
-//         command = power < 0 ? 191 - magnitude : 192 + magnitude;
-//     }
-
-//     //command = constrain(command, 1, 254);
-//     int8_t b = command; 
-//     Serial.print(b); 
-//     LINEAR_ACTUATOR_SERIAL.write(b);  
-// }
 
