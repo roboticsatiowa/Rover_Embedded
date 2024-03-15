06dc37932f022e459db29619075542831b398988
Ethan Holter <holterethan@gmail.com>
2024-03-15 00:19:16 -0500

    changed file extensions from ino to cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,57 +1,6 @@
-/*********************************************************************
- *  ROSArduinoBridge
-
-    A set of simple serial commands to control a differential drive
-    robot and receive back sensor and odometry data. Default
-    configuration assumes use of an Arduino Mega + Pololu motor
-    controller shield + Robogaia Mega Encoder shield.  Edit the
-    readEncoder() and setMotorSpeed() wrapper functions if using
-    different motor controller or encoder method.
-
-    Created for the Pi Robot Project: http://www.pirobot.org
-    and the Home Brew Robotics Club (HBRC): http://hbrobotics.org
-
-    Authors: Patrick Goebel, James Nugen
-
-    Inspired and modeled after the ArbotiX driver by Michael Ferguson
-
-    Software License Agreement (BSD License)
-
-    Copyright (c) 2012, Patrick Goebel.
-    All rights reserved.
-
-    Redistribution and use in source and binary forms, with or without
-    modification, are permitted provided that the following conditions
-    are met:
-
-     * Redistributions of source code must retain the above copyright
-       notice, this list of conditions and the following disclaimer.
-     * Redistributions in binary form must reproduce the above
-       copyright notice, this list of conditions and the following
-       disclaimer in the documentation and/or other materials provided
-       with the distribution.
-
-    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-    LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-    FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-    COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-    BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-    LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-    LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-    ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *********************************************************************/
-
-// ---------------------- DEFINES ----------------------
-
-/* Serial port baud rate */
-#define BAUDRATE 115200
-
-/* Run the PID loop at 30 times per second */
+#define BAUDRATE 115200 // Teensy <---> Jetson
 #define PID_RATE 30 // Hz
+#define AUTO_STOP_INTERVAL 10000 // milliseconds
 
 // ---------------------- INCLUDES ----------------------
 
@@ -67,18 +16,13 @@
 #include "linear_actuator_driver.h"
 #include "stepper_driver.h"
 
+// sensors
 #include "sensors.h"
 #include "pid_controller.h"
 
-/* Convert the rate into an interval */
-const int PID_INTERVAL = 1000 / PID_RATE;
-
-/* Track the next time we make a PID calculation */
+const int PID_INTERVAL = 1000 / PID_RATE; //milliseconds
 unsigned long nextPID = PID_INTERVAL;
 
-/* Stop the robot if it hasn't received a movement command
- in this number of milliseconds */
-#define AUTO_STOP_INTERVAL 10000
 long lastMotorCommand = AUTO_STOP_INTERVAL;
 
 /* Variable initialization */
